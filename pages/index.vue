e718e947865ecf698c0e3d625f81847e8c24971d
thaisc98 <thaisc98@hotmail.es>
2020-12-16 22:58:19 -0400

    [new] charts

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,92 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        telemetry
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <b-container>
+    <b-row>
+      <b-col>
+        <div>
+          <!-- Pilot -->
+        </div>
+      </b-col>
+      <b-col>
+        <div>2 of 3</div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      barChartData: {
+        labels: [
+          "2019-06",
+          "2019-07",
+          "2019-08",
+          "2019-09",
+          "2019-10",
+          "2019-11",
+          "2019-12",
+          "2020-01",
+          "2020-02",
+          "2020-03",
+          "2020-04",
+          "2020-05",
+        ],
+        datasets: [
+          {
+            label: "Visits",
+            data: [10, 15, 20, 30, 40, 50, 60, 70, 34, 45, 11, 78, 45],
+            backgroundColor: "#003f5c",
+          },
+          {
+            label: "Pages Views",
+            data: [30, 24, 57, 23, 68, 72, 25, 64, 133, 143, 165, 33, 56],
+            backgroundColor: "#2f4b7c",
+          },
+          {
+            label: "Users",
+            data: [45, 65, 30, 53, 34, 35, 26, 37, 34, 45, 67, 87, 98],
+            backgroundColor: "#665191",
+          },
+        ],
+      },
+      barChartOptions: {
+        responsive: true,
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: "Google analytics data",
+          fontSize: 24,
+          fontColor: "#6b7280",
+        },
+        tooltips: {
+          backgroundColor: "#17BF62",
+        },
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+      },
+    };
+  },
+};
 </script>
 
 <style>
@@ -42,16 +100,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
